3bd3f967b1d5ff84f8dfc358c5923811bf325f61
m2216 <53843710+m2216@users.noreply.github.com>
2019-08-07 20:23:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = 'a';
+const prefix = '+';
 var table = require('table').table
 var ti={}  
 ,spee={}
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['ahelp','Type ahelp'];
+    var setGame = ['+help','Type ahelp'];
     var i = -1;
     var j = 0;
     setInterval(function (){
@@ -128,8 +128,8 @@ ${prefix}deletecolors <number> / لحذف الالوان
    `,`
         ***__Music orders__***
 **
-${prefix}play / لتشغيل أغنية برآبط أو بأسم
-${prefix}skip / لتجآوز الأغنية الحآلية
+${prefix}شفل / لتشغيل أغنية برآبط أو بأسم
+${prefix}تخطى / لتجآوز الأغنية الحآلية
 ${prefix}pause / إيقآف الأغنية مؤقتا
 ${prefix}resume / لموآصلة الإغنية بعد إيقآفهآ مؤقتا
 ${prefix}vol / لتغيير درجة الصوت 100 - 0
